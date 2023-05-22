0b14005550657b651060e7cb1beffe49e94ed7df
DivyaKarale2 <130248671+DivyaKarale2@users.noreply.github.com>
2023-05-22 23:24:59 +0530

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    document.getElementById("my_audio").play();
+// window.onload = function() {
+//     document.getElementById("my_audio").play();
+// }
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
 }
